daf12a32db9ccbb1f8f77c5f26b7d5a0b7c1c912
Ville Salonen <ville.salonen@iki.fi>
2013-09-11 21:39:08 +0300

    Moved StockSignal class to its own file

---
+++ src/Core/StockSignal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core
+{
+    public class StockSignal
+    {
+        public TickerSymbol Ticker { get; set; }
+        public ISignal Signal { get; set; }
+
+        public int Assess(DateTime time)
+        {
+            return Signal.Assess(Ticker, time);
+        }
+    }
+}
--- src/Core/Trader.cs
+++ src/Core/Trader.cs
@@ -41,15 +41,4 @@ namespace Core
                 _stockbroker.Sell(stockSignal.Ticker, 1);
         }
     }
-
-    public class StockSignal
-    {
-        public TickerSymbol Ticker { get; set; }
-        public ISignal Signal { get; set; }
-
-        public int Assess(DateTime time)
-        {
-            return Signal.Assess(Ticker, time);
-        }
-    }
 }
