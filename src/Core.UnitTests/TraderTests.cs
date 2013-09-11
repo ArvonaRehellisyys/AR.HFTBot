7e2ce4556fa9a258018c72b12120ff4b170d3b0a
Ville Salonen <ville.salonen@iki.fi>
2013-09-11 20:48:42 +0300

    Added test project

---
+++ src/Core.UnitTests/TraderTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Core.UnitTests
+{
+    [TestFixture]
+    public class TraderTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
