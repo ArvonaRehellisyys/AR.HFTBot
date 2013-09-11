b59412790753702269f6bd053844f2e315c329c9
Ville Salonen <ville.salonen@iki.fi>
2013-09-11 21:09:47 +0300

    Implemented first version of Trader

--- src/Core.UnitTests/TraderTests.cs
+++ src/Core.UnitTests/TraderTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Core.UnitTests
 {
@@ -6,9 +8,47 @@ namespace Core.UnitTests
     public class TraderTests
     {
         [Test]
-        public void TestMethod1()
+        public void Trade_BuySignalReceived_BuysStock()
         {
-            Assert.AreEqual(true, true);
+            // Arrange
+            var mockBroker = Substitute.For<IStockbroker>();
+
+            var mockSignal = Substitute.For<ISignal>();
+            mockSignal.Assess(Arg.Any<TickerSymbol>(), Arg.Any<DateTime>())
+                      .Returns(1);
+
+
+            // Act
+            var trader = new Trader(mockBroker);
+            trader.RegisterStock(new TickerSymbol { Name = "AAPL" }, mockSignal);
+            trader.Trade();
+
+
+            // Assert
+            mockBroker.Received().Buy(Arg.Is<TickerSymbol>(x => x.Name == "AAPL"),
+                                      Arg.Is(1));
+        }
+
+        [Test]
+        public void Trade_SellSignalReceived_SellsStock()
+        {
+            // Arrange
+            var mockBroker = Substitute.For<IStockbroker>();
+
+            var mockSignal = Substitute.For<ISignal>();
+            mockSignal.Assess(Arg.Any<TickerSymbol>(), Arg.Any<DateTime>())
+                      .Returns(-1);
+
+
+            // Act
+            var trader = new Trader(mockBroker);
+            trader.RegisterStock(new TickerSymbol { Name = "NOK" }, mockSignal);
+            trader.Trade();
+
+
+            // Assert
+            mockBroker.Received().Sell(Arg.Is<TickerSymbol>(x => x.Name == "NOK"),
+                                       Arg.Is(1));
         }
     }
 }
